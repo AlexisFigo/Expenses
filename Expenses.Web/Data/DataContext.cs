38da9342e1b33354150ca011071ceac5879175e5
Alexis Correa <figo1813@gmail.com>
2020-03-24 19:47:38 -0500

    classes entitis were added

---
+++ Expenses.Web/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Expenses.Web.Data.Entitis;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Expenses.Web.Data
+{
+    public class DataContext:IdentityDbContext<UserEntity>
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+    }
+}
